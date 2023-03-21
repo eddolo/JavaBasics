19cb4c44fd7d8603e25c67d3e251461ee8cdc9e6
paolocappuccini <cappuccinipaolo@gmail.com>
2023-03-21 18:48:57 +0000

    new exercises

--- src/LogicalOr.java
+++ src/LogicalOr.java
@@ -1,2 +1,14 @@
 public class LogicalOr {
+    public class void main (String [] args){
+        System.out.println("Is the number less than 0 OR greater than 100?");
+        int number = 145;
+        if (number < 0 || number > 100)
+        {
+            System.out.println("Yes!");
+        }
+        else
+        {
+            System.out.println("No!");
+        }
+    }
 }
